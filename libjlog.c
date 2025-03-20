98745978cd23f9b66962222e42801d6463b3368c
Bingus <bendawsdev@gmail.com>
2025-03-20 11:15:12 -0500

    v1.1.3

--- libjlog.c
+++ libjlog.c
@@ -7,9 +7,12 @@ libjlog - (C) Copyright 2024 - 2025 Ben Daws. <bendawsdev AT gmail DOT com>
 void configured_callback;
 char configured_system_name[64];
 
+bool is_configured = false;
+
 void libjlog_init(void printing_callback, char *system_name) {
     configured_callback = printing_callback;
     configured_system_name = system_name;
+    is_configured = true;
 }
 
 char *libjlog_output_tostr(enum OUTPUT_TYPE output_type) {
@@ -33,5 +36,6 @@ char *libjlog_output_tostr(enum OUTPUT_TYPE output_type) {
 }
 
 void libjlog_printf(char *PROCESS, enum OUTPUT_TYPE output_type, char *text) {
+    if (!is_configured) return;
     configured_callback(("[%s@%s / %s]: %s", PROCESS, configured_system_name, libjlog_output_tostr(output_type), text));
 }
